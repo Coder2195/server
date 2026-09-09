ac091494652409894ade2222793deb04741b3562
Coder2195 <coder2195mail@gmail.com>
2026-09-09 19:58:49 -0400

    change permission issues

--- minecraft/template.sh
+++ minecraft/template.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cp /template/amber-smp/paper-global.yml /amber-smp/config/paper-global.yml
-cp /template/pumpkin-anarchy/pumpkin.toml /pumpkin-anarchy/pumpkin.toml
-cp /template/amber-smp/server.properties /amber-smp/server.properties
+cp -a /template/amber-smp/paper-global.yml /amber-smp/config/paper-global.yml
+cp -a /template/pumpkin-anarchy/pumpkin.toml /pumpkin-anarchy/pumpkin.toml
+cp -a /template/amber-smp/server.properties /amber-smp/server.properties
 sed -i "s|VELOCITY_SECRET_REPLACE_ME|$(cat /run/secrets/velocity-secret)|g" /amber-smp/config/paper-global.yml
 sed -i "s|VELOCITY_SECRET_REPLACE_ME|$(cat /run/secrets/velocity-secret)|g" /pumpkin-anarchy/pumpkin.toml
 sed -i "s|RCON_PASSWORD_REPLACE_ME|$(cat /run/secrets/rcon-password)|g" /amber-smp/server.properties
